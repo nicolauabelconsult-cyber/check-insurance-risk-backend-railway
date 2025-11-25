dc77ad8dd924203fefac4f52d2b3e58f09768516
Nicolau Abel <nicolauabel.consult@gmail.com>
2025-11-25 18:11:44 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from app.config import settings
-
+from .config import settings
 
 class Base(DeclarativeBase):
     pass
 
-
 engine = create_engine(settings.DATABASE_URL, echo=False, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
-
 
 def get_db():
     db = SessionLocal()
