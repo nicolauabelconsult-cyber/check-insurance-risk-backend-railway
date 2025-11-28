a39028171ed3eda9d98a014224dd03f73827d952
Nicolau Abel <nicolauabel.consult@gmail.com>
2025-11-28 18:44:00 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from typing import Optional
 
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
