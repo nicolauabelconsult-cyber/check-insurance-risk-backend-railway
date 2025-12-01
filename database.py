34b3f231f7712c3cfe0f5afe7a47324b32836e52
Nicolau Abel <nicolauabel.consult@gmail.com>
2025-12-01 09:38:30 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,22 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker, declarative_base
 from config import settings
 
-class Base(DeclarativeBase):
-    pass
+# Detectar se é Postgres do Render
+if settings.DATABASE_URL.startswith("postgres"):
+    engine = create_engine(
+        settings.DATABASE_URL.replace("postgres://", "postgresql://"),
+        pool_pre_ping=True
+    )
+else:
+    engine = create_engine(
+        settings.DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
 
-engine = create_engine(settings.DATABASE_URL, echo=False, future=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
