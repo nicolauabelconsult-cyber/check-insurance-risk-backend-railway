9a4834a4cb8a22ebae0da170ee5fe7002692a8f9
Nicolau Abel <nicolauabel.consult@gmail.com>
2026-03-07 15:29:17 +0100

    Delete app directory

--- app/routers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""API routers package.
-
-Render/Python needs an __init__.py so `app.routers` can be imported.
-"""
-
-from . import auth, entities, users, sources, risks, audit, public  # noqa: F401
